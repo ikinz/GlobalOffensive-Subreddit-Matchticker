c56d41faa45ef1b27e8a81217afc9a1eb8afc4b9
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2014-10-31 09:07:52 +0100

    first commit

---
+++ csgo-matchticker.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        GlobalOffensiveLiveMatchTicker
+// @namespace   globaloffensive-reddit-matchticker
+// @description This will make the matchticker more obvious when using the global offensive subreddit. You will also be able to toggle live scores.
+// @include     *.reddit.com/r/GlobalOffensive/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @version     0.0.1
+// @grant       none
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+$(document).ready(function() {
+    var newdiv = $("<li></li>").append("<a id='showticker' href='#'>Show Matchticker</a>");
+	$('.tabmenu').append(newdiv);
+});
