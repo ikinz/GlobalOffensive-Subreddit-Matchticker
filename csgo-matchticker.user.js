072c4b4e85b4cb21c4cb0c324efb0508436a8b91
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2014-10-31 22:44:43 +0100

    Get RSS done

--- csgo-matchticker.user.js
+++ csgo-matchticker.user.js
@@ -4,13 +4,22 @@
 // @description This will make the matchticker more obvious when using the global offensive subreddit. You will also be able to toggle live scores.
 // @include     *.reddit.com/r/GlobalOffensive/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version     0.0.1
-// @grant       none
+// @version     0.0.2
+// @grant 		GM_xmlhttpRequest
+// @grant		GM_addStyle
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
+var tickerShowing = false;
+
 $(document).ready(function() {
+	GM_addStyle(
+		".ticker-match {" +
+			"width: 10%;" +
+		"}"
+	);
+	
 	createElements();
 });
 
@@ -20,11 +29,13 @@ function setLSShowTicker() {
 
 function createElements() {
 	var tickerdiv = $("<div id='custommatchticker'></div>");
-	tickerdiv.is(":hidden");
-	$('.content').prepend(tickerdiv);
+	tickerdiv.addClass("link");
+	//tickerdiv.is(":hidden");
+	tickerdiv.slideUp("fast");
+	$(tickerdiv).prependTo('#siteTable');
 	
 	var menudiv = $("<li></li>").append("<a id='showticker' href='#'>Show Matchticker</a>");
-	$('.tabmenu').append(menudiv);
+	$(menudiv).appendTo('.tabmenu');
 	$("#showticker").on("click", function(e) {
 		e.preventDefault();
 		displayMatchTicker();
@@ -40,5 +51,28 @@ function createElements() {
 }
 
 function displayMatchTicker() {
-	$('.content:first-child').append($('<p>test</p>'));
+	var div = $('#siteTable > div').first();
+	div.text("test");
+	
+	if (tickerShowing) {
+		div.slideUp("fast");
+		tickerShowing = false;
+	} else {
+		div.slideDown("fast");
+		tickerShowing = true;
+	}
+	
+	var hltvrss = "http://www.hltv.org/hltv.rss.php";
+	GM_xmlhttpRequest({
+		method: "GET",
+		url: hltvrss,
+		data: "pri=15",
+		headers: {
+			"User-Agent": "Mozilla/5.0",    // If not specified, navigator.userAgent will be used.
+			"Accept": "text/xml"            // If not specified, browser defaults will be used.
+		},
+		onload: function(data) {
+			alert(data.responseText);
+		}
+	});
 }
