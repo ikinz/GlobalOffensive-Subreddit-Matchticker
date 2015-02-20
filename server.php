35a50b4214c6693098c594ccfd5d9b662d7c8dc3
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-02-20 02:11:26 +0100

    fixed mysql connection

--- server.php
+++ server.php
@@ -1,5 +1,10 @@
 <?php
-    include mysqlconf.php;
+    include('mysqlconf.php');
+
+    $conn = new mysqli($servername, $username, $password, $db);
+    if ($conn->connect_error) {
+        die("Connection failed: ".$conn->connect_error);
+    }
 
     if (isset($_GET['matchid'])) {
         
